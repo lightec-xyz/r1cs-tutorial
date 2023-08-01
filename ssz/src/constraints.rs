43e9774566ab4d8bf14ba8724eea0d6deb6d7500
keep <readygo67@github.com>
2023-08-01 22:00:36 +0800

    add sha256bytes gadget and circuit

--- ssz/src/constraints.rs
+++ ssz/src/constraints.rs
@@ -89,65 +89,6 @@ impl <ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF> for Sha256Byte
 }
 
 
-#[test]
-fn test_crh_sha256() {
-
-    use ark_relations::{
-        ns,
-        r1cs::{ConstraintSystem, Namespace}
-    };
-    use ark_ed_on_bls12_381::Fr;
-
-    // let mut rng = ark_std::test_rng();
-    let cs = ConstraintSystem::<Fr>::new_ref();
-
-    fn to_byte_vars(cs: impl Into<Namespace<Fr>>, data: &[u8]) -> Vec<UInt8<Fr>> {
-        let cs = cs.into().cs();
-        UInt8::new_witness_vec(cs, data).unwrap()
-    }
-
-    // CRH parameters are nothing, 
-    let unit = (); //sha256的空parameter
-    let unit_var = UnitVar::default(); //sha256Gadget的空parameter
-
-    let input_str = vec![0u8; 64]; 
-
-    // Compute the hashes and assert consistency
-    let computed_output = <Sha256Gadget<Fr> as CRHSchemeGadget<Sha256, Fr>>::evaluate(
-        &unit_var,
-        &to_byte_vars(ns!(cs, "input"), &input_str),
-    )
-    .unwrap();
-    let expected_output = <Sha256 as CRHScheme>::evaluate(&unit, input_str).unwrap();
-    println!("expected_output {:?}", expected_output);
-    dbg!(&computed_output.value().unwrap().to_vec());
-    assert_eq!(
-        computed_output.value().unwrap().to_vec(),
-        expected_output,
-        "CRH error at length"
-    )
-
-    // for &len in TEST_LENGTHS {
-    //     // Make a random string of the given length
-    //     let mut input_str = vec![0u8; len];
-    //     rng.fill_bytes(&mut input_str);
-
-    //     // Compute the hashes and assert consistency
-    //     let computed_output = <Sha256Gadget<Fr> as CRHSchemeGadget<Sha256, Fr>>::evaluate(
-    //         &unit_var,
-    //         &to_byte_vars(ns!(cs, "input"), &input_str),
-    //     )
-    //     .unwrap();
-    //     let expected_output = <Sha256 as CRHScheme>::evaluate(&unit, input_str).unwrap();
-    //     assert_eq!(
-    //         computed_output.value().unwrap().to_vec(),
-    //         expected_output,
-    //         "CRH error at length {}",
-    //         len
-    //     )
-    // }
-}
-
 
 #[test] 
 // 测试sha256BytesGadget
